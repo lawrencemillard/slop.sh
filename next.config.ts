5db62f713f0fb03d2a2b3a97346c3296e1399b17
Keiran <keiran0@proton.me>
2025-03-29 22:35:27 +0000

    added route handler for alias redirects

--- next.config.ts
+++ next.config.ts
@@ -1,25 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  redirects: async () => {
-    return [
-      {
-        source: "/bio",
-        destination: "https://e-z.bio/kc",
-        permanent: true,
-      },
-      {
-        source: "/sky",
-        destination: "https://sky.shiiyu.moe/stats/q_ez/Pomegranate",
-        permanent: true,
-      },
-      {
-        source: "/github",
-        destination: "https://github.com/q4ow",
-        permanent: true,
-      },
-    ];
-  },
   images: {
     remotePatterns: [
       {
